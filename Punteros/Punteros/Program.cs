d86e120e47e18f883ad8214abf4385ac201f0a8d
Lex <116053579+Lexioor@users.noreply.github.com>
2024-02-19 07:56:26 -0600

    Ejemplo Punteros
    Ejemplo pactico sobre los Smart Pointers del alumno Martínez Camarena Lex Cristofer #23310152 2-P

---
+++ Punteros/Punteros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Resource : IDisposable
+{
+    public Resource()
+    {
+        Console.WriteLine("Puntero Creado");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Puntero Borrado");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var resource = new Resource())
+        {
+
+            Console.WriteLine("Puntero en uso ");
+        }
+    }
+}
