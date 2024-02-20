1f98de98849201d0ae5735f037720a6ac088efed
Lex <116053579+Lexioor@users.noreply.github.com>
2024-02-19 21:55:25 -0600

    Update main.cpp

--- POO 2/main.cpp
+++ POO 2/main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include "Player/Player.h"
 #include "Enemy/Enemy.h"
-#include "Combat/Combat.h"
 
 int main() {
-    Player *player = new Player("Victor", 100, 8, 4, 3);
-    Enemy *enemy = new Enemy("Estudiante de Redes", 50, 5, 2, 5, 10);
+    while(true) {
+        Player *player = new Player("Lexioor", 100, 8, 4, 10);
+        Enemy *enemy = new Enemy("SAT", 50, 5, 2, 5, 10);
 
-    vector<Character*> participants;
+        cout << player->toString() << endl;
+        cout << enemy->toString() << endl;
 
-    participants.push_back(player);
-    participants.push_back(enemy);
+        cout << "=====================" << endl;
 
-    Combat *combat = new Combat(participants);
-    combat->doCombat();
+        while(player->isAlive() && enemy->isAlive()) {
+            player->doAttack(enemy);
+            if (!enemy->isAlive()) break; // Si el enemigo muere, salimos del bucle
+            enemy->doAttack(player);
+            if (!player->isAlive()) break; // Si el jugador muere, salimos del bucle
+        }
 
-    delete player;
-    delete enemy;
-    delete combat;
+        cout << player->toString() << endl;
+        cout << enemy->toString() << endl;
+
+        if (!player->isAlive()) {
+            cout << "El jugador ha muerto. Fin del juego." << endl;
+            delete player;
+            delete enemy;
+            break; // Salimos del bucle principal
+        } else if (!enemy->isAlive()) {
+            cout << "El enemigo ha muerto. Continuamos con el siguiente enemigo." << endl;
+            delete player;
+            delete enemy;
+        }
+    }
     return 0;
 }
